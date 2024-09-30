574159b0031c37dcee001a6260467b2e33bcf9c7
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-09-30 16:46:16 +0300

    2FA start

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,9 +1,36 @@
+import crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
 
 import { db } from '@/lib/db';
 
 import { getVerificationTokenByEmail } from '@/data/verification-token';
 import { getResetPasswordTokenByEmail } from '@/data/password-reset-token';
+
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  // TODO: current expire time is 1 hour, you can change like you want
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+  return twoFactorToken;
+};
 
 export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4();
